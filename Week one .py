45c693b824f0f89986fb4559d3f12f56e1774ca3
Paul Djangang <60377406+Djangang@users.noreply.github.com>
2020-02-02 23:38:37 -0700

    initial commit

---
+++ Week one .py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Create a new repository on your Github account and name it Coursera_Capstone. I have prepared a guide to walk you through the process of creating a repository and setting it up. For Mac users, click here. For Windows users, click here.
+# 
+# ## Now, start a Jupyter Notebook using any platform that you are comfortable with and do the following:
+# 
+# ## Write some markdown to explain that this notebook will be mainly used for the capstone project.
+# #### 1- Import the pandas library as pd.
+# #### 2- Import the Numpy library as np.
+# #### 3- Print the following the statement: Hello Capstone Project Course!
+# #### 4- Push the Notebook to your Github repository and submit a link to the notebook on your Github repository.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
